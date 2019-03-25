3baf3db847aed71b72de6dcb568e7464eb28b2ef
Mattias Rådemar <mattiasrademar@gmail.com>
2019-03-25 09:32:37 +0100

    first commit

---
+++ src/components/Field/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './field.css'
+
+class Field extends Component {
+	render() {
+		return (
+			<div>
+				<img src="./football-field.png" alt="" />
+			</div>
+		)
+	}
+}
+
+export default Field
